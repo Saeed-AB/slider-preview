8e9dbcc1e21dbcf75f4685a8690f8424cced98d1
Saeed beraia <saeed.beraia1996@gmail.com>
2025-04-12 00:05:16 +0300

    config rollbar

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import viteRollbar from "vite-plugin-rollbar";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  server: {
-    port: 3003,
-  },
+  const rollbarConfig = {
+    accessToken: env.VITE_ROLLBAR_POST_ACCESS_TOKEN,
+    version: "1.0.0",
+    baseUrl: env.VITE_ROLLBAR_BASE_URL,
+    ignoreUploadErrors: true,
+    silent: true,
+  };
+
+
+  return {
+    plugins: [svgr(), react(), tsconfigPaths(), viteRollbar(rollbarConfig)],
+    server: {
+      port: 3000,
+    },
+    build: {
+      sourcemap: true,
+    },
+  };
 });
